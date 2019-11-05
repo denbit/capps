739bce9ec87a80b0fdbacf8d07404a358dff6a94
denbitv2 <denys.b@cudev.org>
2019-11-05 09:45:17 +0200

    file 1

--- register_var.c
+++ register_var.c
@@ -4,9 +4,31 @@
  * and open the template in the editor.
  */
 #include <stdio.h>
+#include <time.h>
 #include "register_var.h"
 
+time_t timer;
+void * p;
+void init_timer(){
+    
+     timer = clock();
+} 
+void print_diff(void){
+    double diff = clock()-timer;
+    printf("%f \n",diff);
+    
+}
     void register_var_test(void)
-    {
-        puts("ha");
+    {CLOCKS_PER_SEC;
+        puts("register var");
+        init_timer();
+        register long reg =0;
+        for(long i=9000000000; i>reg;reg++);
+        print_diff();
+        puts("regular var");        
+        init_timer();
+        long nonreg =0;
+        for(long ireg=9000000000; ireg>nonreg;nonreg++);
+        print_diff();
+        
     }
